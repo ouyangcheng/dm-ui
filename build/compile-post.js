6bba3bb5fc7e81403fd130d222165d0399d363e6
yuwen <zengyuwen@dianmi365.com>
2017-05-09 16:33:23 +0800

    0.1.4-0

--- build/compile-post.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by otherlite on 16/9/6.
- */
-const chalk = require('chalk');
-console.log(chalk.yellow('============compile:post============'))
-console.log(chalk.yellow('============end============'))
