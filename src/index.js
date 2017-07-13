b41d47e06a36556dba8165dca54b9b9ed9cda8fd
yuwen <zengyuwen@dianmi365.com>
2017-07-13 16:30:13 +0800

    1.0.0-9

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ import './style/index.scss'
 import richEditor from './components/richEditor/richEditor.vue'
 
 const dmui = {
-  richEditor
+    richEditor
 }
 
-const install = function (Vue, opts = {}) {
-  Vue.use(iview)
-  Object.keys(dmui).forEach((key) => {
-    Vue.component(key, dmui[key])
-  })
+const install = function(Vue, opts = {}) {
+    Vue.use(iview)
+    Object.keys(dmui).forEach((key) => {
+        Vue.component(key, dmui[key])
+    })
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
+    install(window.Vue)
 }
 
 module.exports = Object.assign(dmui, {
-  install
+    install
 })
